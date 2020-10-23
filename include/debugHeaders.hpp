7875f4f4d0cb86193a6598e6abfec9a9dd35d0f9
aravindSwamy94 <s.aravind4@gmail.com>
2020-10-23 11:51:28 +0300

    Fix minor issues

--- include/debugHeaders.hpp
+++ include/debugHeaders.hpp
@@ -1,4 +1,4 @@
-#define FIND_ROBOT_DEBUG_PLOT 1 // Debug the Find robot
+#define FIND_ROBOT_DEBUG_PLOT 0 // Debug the Find robot
 #define DEBUG_PROCESS_MAP 0 // Debug the process map
 #if DEBUG_PROCESS_MAP
     #define FIND_OBSTACLES_DEBUG_PLOT 1 // Visualize the obstacles in debug image of process map
