5cc2fb82587c60935884922757b0f66b7f3a881c
aravindSwamy94 <s.aravind4@gmail.com>
2021-01-14 21:25:26 +0200

    Clean code

--- include/debugHeaders.hpp
+++
@@ -1,8 +0,0 @@
-#define FIND_ROBOT_DEBUG_PLOT 0 // Debug the Find robot
-#define DEBUG_PROCESS_MAP 0 // Debug the process map
-#if DEBUG_PROCESS_MAP
-    #define FIND_OBSTACLES_DEBUG_PLOT 1 // Visualize the obstacles in debug image of process map
-    #define FIND_GATE_DEBUG_PLOT 1 // Visualize the Gate in debug image of process map
-    #define FIND_VICTIMS_DEBUG_PLOT 1 // Visualize the Victims in debug image of process map
-#endif
-#define DEBUG_VICTIM_ID 0 // // Debug the Victim ID in debug image 
